273f37e6da5538683f63bbecaf0d9a054640ac25
ArturoP04 <115001628+ArturoP04@users.noreply.github.com>
2022-12-15 10:47:50 +0100

    Primer commit, creat el codi.

---
+++ Retiraada_de_Cotxes/src/retiraada_de_cotxes/Retiraada_de_Cotxes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package retiraada_de_cotxes;
+
+/**
+ *
+ * @author arturo.perez
+ */
+public class Retiraada_de_Cotxes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
